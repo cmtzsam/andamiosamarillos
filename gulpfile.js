b00b7dcaf9fdcbf1e9df08a755b4d3bb2377d05b
cmtzsam <52460157+cmtzsam@users.noreply.github.com>
2025-04-15 13:51:18 -0600

    fix img plugin error and fonts error

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,9 @@ const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
 
 // Imagemin desde Node
-const imagemin = require('imagemin');
+// const imagemin = require('imagemin');
 // const imageminMozjpeg = require('imagemin-mozjpeg');
-const imageminPngquant = require('imagemin-pngquant');
+// const imageminPngquant = require('imagemin-pngquant');
 // const imageminSvgo = require('imagemin-svgo');
 const path = require('path');
 
@@ -26,10 +26,10 @@ const paths = {
     src: 'assets/js/**/*.js',
     dest: 'assets/js/min/'
   },
-  images: {
-    src: 'assets/images/**/*.{jpg,jpeg,png,svg}',
-    dest: 'assets/images/compressed/'
-  },
+  // images: {
+  //   src: 'assets/images/**/*.{jpg,jpeg,png,svg}',
+  //   dest: 'assets/images/compressed/'
+  // },
   html: {
     src: 'index.html'
   }
@@ -57,30 +57,7 @@ function scripts() {
     .pipe(browserSync.stream());
 }
 
-// Optimize images using imagemin from Node
-async function images(done) {
-  const files = await globby(paths.images.src);
 
-  try {
-    await Promise.all(
-      files.map((file) =>
-        imagemin([file], {
-          destination: paths.images.dest,
-          plugins: [
-            // imageminMozjpeg({ quality: 75 }),
-            imageminPngquant({ quality: [0.6, 0.8] }),
-            // imageminSvgo()
-          ]
-        })
-      )
-    );
-    console.log('✔ Imágenes optimizadas');
-    done();
-  } catch (err) {
-    console.error('✘ Error optimizando imágenes:', err);
-    done(err);
-  }
-}
 
 // Live reload server
 function serve() {
@@ -92,12 +69,12 @@ function serve() {
 
   gulp.watch(paths.styles.src, styles);
   gulp.watch(paths.scripts.src, scripts);
-  gulp.watch(paths.images.src, images);
+  // gulp.watch(paths.images.src, images);
   gulp.watch(paths.html.src).on('change', browserSync.reload);
 }
 
 // Default task
 exports.default = gulp.series(
-  gulp.parallel(styles, scripts, images),
+  gulp.parallel(styles, scripts),
   serve
 );
